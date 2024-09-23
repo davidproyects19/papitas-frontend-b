29f1fe1105116720ae9025f273c010de6c627b54
DAVID <fernandez.davidgoza@gmail.com>
2024-09-23 17:06:06 -0500

    Carga inical del proyecto

---
+++ src/main/java/com/example/patitas_frontend_b/PatitasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package com.example.patitas_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrontendBApplication.class, args);
+	}
+
+}
